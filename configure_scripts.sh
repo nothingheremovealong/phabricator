118d6d6d52d63930d19ae95077d8b74acd63f516
Jeff Verkoeyen <featherless@google.com>
2015-11-15 17:01:14 -0500

    Fix bug.

--- configure_scripts.sh
+++ configure_scripts.sh
@@ -4,14 +4,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function safe_copy {
   if [ ! -f $2 ]; then
-    echo "Copying $1..."
+    echo "Copying $1 to $2..."
     cp $1 $2
   else
     if ! cmp --silent $1 $2; then
       echo "Overwrite existing $1 at $2?"
       select yn in "Yes" "No"; do
         case $yn in
-          Yes ) echo "Updating $2 site...";cp $1 $2; break;;
+          Yes ) echo "Copying $1 to $2...";cp $1 $2; break;;
           No ) exit;;
         esac
       done
