6ed5c37293f83daf9daff2e06f12bc6e11ade5be
Jeff Verkoeyen <featherless@google.com>
2015-11-16 02:08:34 -0500

    jfsld

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+if [ "$#" -lt 1 ]; then
+  echo "Usage: ${BASH_SOURCE[0]} <project_name>"
+  exit 1
+fi
+
+PROJECT=$1
+
+if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep phabricator)" ]; then
+  gcloud --quiet --project="${PROJECT}" sql instances create "phabricator" \
+    --backup-start-time="00:00" \
+    --assign-ip \
+    --authorized-networks "0.0.0.0/0" \
+    --authorized-gae-apps "${PROJECT}" \
+    --gce-zone "us-central" \
+    --tier="D1" \
+    --pricing-plan="PACKAGE" \
+    --database-flags="sql_mode=STRICT_ALL_TABLES,ft_min_word_len=3,max_allowed_packet=33554432"
+fi
+
