582b814a65bae0beb993137dba26979895aa36e9
Jeff Verkoeyen <featherless@google.com>
2015-11-15 20:10:31 -0500

    Fix.

--- configure_sendgrid.sh
+++ configure_sendgrid.sh
@@ -2,24 +2,22 @@
 
 sudo apt-get install postfix libsasl2-modules -y
 
-/etc/postfix/main.cf
-
-if [ $(grep -c "^default_transport" /etc/postfix/main.cf) -ne 0  ]; then
+if [ $(grep -c "^default_transport" /etc/postfix/main.cf) -ne 0 ]; then
   echo "Disabling default_transport...";
   sed -i -e "s/^default_transport/# default_transport/" /etc/postfix/main.cf
 fi
 
-if [ $(grep -c "^relay_transport" /etc/postfix/main.cf) -ne 0  ]; then
+if [ $(grep -c "^relay_transport" /etc/postfix/main.cf) -ne 0 ]; then
   echo "Disabling relay_transport...";
   sed -i -e "s/^relay_transport/# relay_transport/" /etc/postfix/main.cf
 fi
 
-if [ $(grep -c "^relayhost" /etc/postfix/main.cf) -eq 0  ]; then
+if [ $(grep -c "^relayhost" /etc/postfix/main.cf) -eq 0 ]; then
   echo "Adding relayhost...";
   echo "relayhost = [smtp.sendgrid.net]:2525" >> /etc/postfix/main.cf
 fi
 
-if [ $(grep -c "^smtp_tls_security_level" /etc/postfix/main.cf) -eq 0  ]; then
+if [ $(grep -c "^smtp_tls_security_level" /etc/postfix/main.cf) -eq 0 ]; then
   echo "Adding smtp_tls_security_level...";
   cat > /etc/postfix/main.cf echo << EOF
 smtp_tls_security_level = encrypt
@@ -27,7 +25,7 @@ smtp_sasl_auth_enable = yes
 smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
 header_size_limit = 4096000
 smtp_sasl_security_options = noanonymous
-  EOF
+EOF
 fi
 
 if [ -f /etc/postfix/sasl_passwd.db ]; then
