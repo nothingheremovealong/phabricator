0cb1e7a4352007748f3fea9d43116392547437fb
Jeff Verkoeyen <featherless@google.com>
2015-11-16 02:13:48 -0500

    Get list.

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -19,3 +19,13 @@ if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep phabric
     --database-flags="sql_mode=STRICT_ALL_TABLES,ft_min_word_len=3,max_allowed_packet=33554432"
 fi
 
+INSTANCE_NAME=$(gcloud --project="${PROJECT}" --quiet sql instances list | grep phabricator | cut -d " " -f 1)
+if [ -z "${INSTANCE_NAME}" ]; then
+  # We could not load the name of the Cloud SQL instance, so we need to bail out.
+  echo "Failed to load the name of the Cloud SQL instance to use for Phabricator"
+  exit
+fi
+
+echo $INSTANCE_NAME
+
+
