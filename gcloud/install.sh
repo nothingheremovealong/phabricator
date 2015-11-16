c3960f15b67038b246ac5067466ee3454bad6fa2
Jeff Verkoeyen <jverkoey@gmail.com>
2015-11-16 10:31:49 -0500

    Gresp.

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -8,18 +8,19 @@ fi
 PROJECT=$1
 
 NETWORK_NAME=phabricator
+SQL_NAME=phabricator
 VM_NAME=phabricator
 
 echo "Network..."
 
-if [ -z "$(gcloud --project=${PROJECT} --quiet compute networks list | grep $NETWORK_NAME)" ]; then
+if [ -z "$(gcloud --project=${PROJECT} --quiet compute networks list | grep "\b$NETWORK_NAME\b")" ]; then
   echo "- Creating $NETWORK_NAME network..."
   gcloud --project="${PROJECT}" --quiet compute networks create $NETWORK_NAME --range "10.0.0.0/24" || exit 1
 fi
 
 echo " Firewall rules..."
 
-if [ -z "$(gcloud --project=${PROJECT} --quiet compute firewall-rules list | grep $NETWORK_NAME | grep allow-internal)" ]; then
+if [ -z "$(gcloud --project=${PROJECT} --quiet compute firewall-rules list | grep "\b$NETWORK_NAME\b" | grep "\ballow-internal\b")" ]; then
 echo " - Creating internal $NETWORK_NAME firewall rules..."
   gcloud --project="${PROJECT}" --quiet compute firewall-rules create allow-internal \
     --allow "tcp:0-65535" \
@@ -27,7 +28,7 @@ echo " - Creating internal $NETWORK_NAME firewall rules..."
     --source-ranges "10.0.0.0/24" || exit 1
 fi
 
-if [ -z "$(gcloud --project=${PROJECT} --quiet compute firewall-rules list | grep $NETWORK_NAME | grep allow-ssh)" ]; then
+if [ -z "$(gcloud --project=${PROJECT} --quiet compute firewall-rules list | grep "\b$NETWORK_NAME\b" | grep "\ballow-ssh\b")" ]; then
 echo " - Creating temporary $NETWORK_NAME ssh firewall rules..."
   gcloud --project="${PROJECT}" --quiet compute firewall-rules create allow-ssh \
     --allow "tcp:22" \
@@ -37,9 +38,9 @@ fi
 
 echo "SQL..."
 
-if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep phabricator)" ]; then
-echo "- Creating phabricator SQL database..."
-  gcloud --quiet --project="${PROJECT}" sql instances create "phabricator" \
+if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep "\b$SQL_NAME\b")" ]; then
+echo "- Creating $SQL_NAME SQL database..."
+  gcloud --quiet --project="${PROJECT}" sql instances create "$SQL_NAME" \
     --backup-start-time="00:00" \
     --assign-ip \
     --authorized-networks "0.0.0.0/0" \
@@ -50,7 +51,7 @@ echo "- Creating phabricator SQL database..."
     --database-flags="sql_mode=STRICT_ALL_TABLES,ft_min_word_len=3,max_allowed_packet=33554432" || exit 1
 fi
 
-SQL_INSTANCE_NAME=$(gcloud --project="${PROJECT}" --quiet sql instances list | grep phabricator | cut -d " " -f 1)
+SQL_INSTANCE_NAME=$(gcloud --project="${PROJECT}" --quiet sql instances list | grep "\b$SQL_NAME\b" | cut -d " " -f 1)
 if [ -z "${SQL_INSTANCE_NAME}" ]; then
   # We could not load the name of the Cloud SQL instance, so we need to bail out.
   echo "Failed to load the name of the Cloud SQL instance to use for Phabricator"
@@ -59,7 +60,7 @@ fi
 
 echo "Compute instances..."
 
-if [ -z "$(gcloud --quiet --project=${PROJECT} compute instances list | grep $VM_NAME)" ]; then
+if [ -z "$(gcloud --quiet --project=${PROJECT} compute instances list | grep "\b$VM_NAME\b")" ]; then
 echo "- Creating $VM_NAME compute instance..."
   gcloud --quiet --project="${PROJECT}" compute instances create "$VM_NAME" \
     --boot-disk-size "10GB" \
@@ -69,7 +70,7 @@ echo "- Creating $VM_NAME compute instance..."
     --zone "us-central1-a" || exit 1
 
   echo " Waiting for compute instance to activate..."
-  while [ -z "$(gcloud --quiet --project=${PROJECT} compute instances list | grep phabricator)" ]; do
+  while [ -z "$(gcloud --quiet --project=${PROJECT} compute instances list | grep "\b$VM_NAME\b")" ]; do
     sleep 10
   done
 fi
