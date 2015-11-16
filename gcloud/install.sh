4d09c4fe4843876806df8854d62ecd50424cae0a
Jeff Verkoeyen <jverkoey@gmail.com>
2015-11-16 02:23:21 -0500

    Checkpoint.

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -7,15 +7,27 @@ fi
 
 PROJECT=$1
 
-# Private network
+echo Network...
 
 if [ -z "$(gcloud --project=${PROJECT} --quiet compute networks list | grep phabricator)" ]; then
-  gcloud --project="${PROJECT}" --quiet compute networks create phabricator --range "10.0.0.0/24"
+  echo Creating phabricator network...
+  gcloud --project="${PROJECT}" --quiet compute networks create phabricator --range "10.0.0.0/24" || exit 1
 fi
 
-# SQL
+echo Firewall rules...
+
+if [ -z "$(gcloud --project=${PROJECT} --quiet compute firewall-rules list | grep phabricator)" ]; then
+echo Creating phabricator firewall rules...
+  gcloud --project="${PROJECT}" --quiet compute firewall-rules create allow-internal \
+    --allow "tcp:0-65535" \
+    --network phabricator \
+    --source-ranges "10.0.0.0/24" || exit 1
+fi
+
+echo SQL...
 
 if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep phabricator)" ]; then
+echo Creating phabricator SQL database...
   gcloud --quiet --project="${PROJECT}" sql instances create "phabricator" \
     --backup-start-time="00:00" \
     --assign-ip \
@@ -24,7 +36,7 @@ if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep phabric
     --gce-zone "us-central1-a" \
     --tier="D1" \
     --pricing-plan="PACKAGE" \
-    --database-flags="sql_mode=STRICT_ALL_TABLES,ft_min_word_len=3,max_allowed_packet=33554432"
+    --database-flags="sql_mode=STRICT_ALL_TABLES,ft_min_word_len=3,max_allowed_packet=33554432" || exit 1
 fi
 
 SQL_INSTANCE_NAME=$(gcloud --project="${PROJECT}" --quiet sql instances list | grep phabricator | cut -d " " -f 1)
