11ac3ccb02a937f243d05027255830439a50f23d
Jeff Verkoeyen <featherless@google.com>
2015-11-16 02:15:07 -0500

    Network.

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -7,6 +7,14 @@ fi
 
 PROJECT=$1
 
+# Private network
+
+if [ -z "$(gcloud --project=${PROJECT} --quiet compute networks list | grep phabricator)" ]; then
+  gcloud --project="${PROJECT}" --quiet compute networks create phabricator --range "10.0.0.0/24"
+fi
+
+# SQL
+
 if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep phabricator)" ]; then
   gcloud --quiet --project="${PROJECT}" sql instances create "phabricator" \
     --backup-start-time="00:00" \
@@ -19,13 +27,9 @@ if [ -z "$(gcloud --quiet --project=${PROJECT} sql instances list | grep phabric
     --database-flags="sql_mode=STRICT_ALL_TABLES,ft_min_word_len=3,max_allowed_packet=33554432"
 fi
 
-INSTANCE_NAME=$(gcloud --project="${PROJECT}" --quiet sql instances list | grep phabricator | cut -d " " -f 1)
-if [ -z "${INSTANCE_NAME}" ]; then
+SQL_INSTANCE_NAME=$(gcloud --project="${PROJECT}" --quiet sql instances list | grep phabricator | cut -d " " -f 1)
+if [ -z "${SQL_INSTANCE_NAME}" ]; then
   # We could not load the name of the Cloud SQL instance, so we need to bail out.
   echo "Failed to load the name of the Cloud SQL instance to use for Phabricator"
   exit
 fi
-
-echo $INSTANCE_NAME
-
-
