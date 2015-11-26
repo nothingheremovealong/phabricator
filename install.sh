784b7c9e3bc46e1a109c587e7fcb8712a0f86ecc
Jeff Verkoeyen <featherless@google.com>
2015-11-26 13:24:46 -0500

    @ for toplevel

--- install.sh
+++ install.sh
@@ -129,7 +129,7 @@ if [ -n $CUSTOM_DOMAIN ]; then
   if [ -z "$(gcloud --project=${PROJECT} --quiet dns record-sets --zone="$DNS_NAME" list | grep "v=spf1 include:mailgun.org ~all")" ]; then
     echo " Adding DNS TXT entry 'v=spf1 include:mailgun.org ~all'..."
     gcloud --project=${PROJECT} --quiet dns record-sets transaction start --zone=$DNS_NAME
-    gcloud --project=${PROJECT} --quiet dns record-sets transaction add --zone=$DNS_NAME --name="$TOP_LEVEL_DOMAIN." --ttl=21600 --type=TXT "v=spf1 include:mailgun.org ~all"
+    gcloud --project=${PROJECT} --quiet dns record-sets transaction add --zone=$DNS_NAME --name="@" --ttl=21600 --type=TXT "v=spf1 include:mailgun.org ~all"
     gcloud --project=${PROJECT} --quiet dns record-sets transaction execute --zone=$DNS_NAME
     echo OK
   fi
