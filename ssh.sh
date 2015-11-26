a5d802708bccb490abb473831ca79cd924261779
Jeff Verkoeyen <featherless@google.com>
2015-11-25 21:46:22 -0500

    Add ssh script.

---
+++ ssh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: ${BASH_SOURCE[0]} <project_name>"
+  exit 1
+fi
+
+PROJECT=$1
+
+# TODO: Find a good way to share this.
+NETWORK_NAME=phabricator
+VM_NAME=phabricator
+
+if [ -z "$(gcloud --project=${PROJECT} --quiet compute firewall-rules list | grep "\b$NETWORK_NAME\b" | grep "\btemp-allow-ssh\b")" ]; then
+  echo "Creating temporary $NETWORK_NAME ssh firewall rule..."
+  gcloud --project="${PROJECT}" --quiet compute firewall-rules create temp-allow-ssh \
+    --allow "tcp:22" \
+    --network $NETWORK_NAME \
+    --source-ranges "0.0.0.0/0" || exit 1
+fi
+
+gcloud --project=${PROJECT} compute ssh $VM_NAME --zone us-central1-a
+
+if [ "$(gcloud --project=${PROJECT} --quiet compute firewall-rules list | grep "\b$NETWORK_NAME\b" | grep "\btemp-allow-ssh\b")" ]; then
+  echo -n "Removing temporary $NETWORK_NAME ssh firewall rule..."
+  gcloud --project="${PROJECT}" --quiet compute firewall-rules delete temp-allow-ssh || exit 1
+fi
