90ec1c205175d4d37cd65ee95e7943cad9d6556e
Jeff Verkoeyen <featherless@google.com>
2015-11-26 16:10:44 -0500

    Touch logs.

--- vm/configure_notifications.sh
+++ vm/configure_notifications.sh
@@ -7,9 +7,7 @@ sudo apt-get install -y npm
 ln -s /usr/bin/nodejs /usr/bin/node
 
 pushd phabricator/support/aphlict/server >> /dev/null
-
 npm install ws
-
 popd >> /dev/null
 
 # Start the notification server
@@ -18,6 +16,9 @@ pushd phabricator >> /dev/null
 ./bin/config set notification.enabled true
 ./bin/config set notification.client-uri http://localhost/ws/
 
-sudo su www -c "bin/aphlict restart --client-host=localhost"
+touch /var/log/aphlict.log
+chmod a+w /var/log/aphlict.log
+
+#bin/aphlict restart --client-host=localhost
 
 popd >> /dev/null
