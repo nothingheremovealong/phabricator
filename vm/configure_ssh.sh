26a48f3d126473f4776107f9d8457b14b3766b43
Jeff Verkoeyen <featherless@google.com>
2015-11-27 10:26:13 -0500

    Only reboot sshd on config changes.

--- vm/configure_ssh.sh
+++ vm/configure_ssh.sh
@@ -3,6 +3,6 @@
 if [ $(grep -c "^Port 22$" /usr/sbin/sshd) -ne 0  ]; then
   echo "Listening port set to 222.";
   sudo sed -i -e 's/^Port 22$/Port 222/' /usr/sbin/sshd
-fi
 
-sudo service ssh restart
+  sudo service ssh restart
+fi
