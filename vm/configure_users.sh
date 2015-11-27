858d435471f4e87525124db63203800e30bdbae5
Jeff Verkoeyen <featherless@google.com>
2015-11-27 02:25:20 -0500

    Configuring users.

--- vm/configure_users.sh
+++ vm/configure_users.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if ! cat /etc/passwd | grep "^phabricator-daemon" >> /dev/null; then
+  sudo useradd -r -s /bin/bash phabricator-daemon
+fi
+
 if ! cat /etc/passwd | grep "^git" >> /dev/null; then
-  sudo useradd -r -s /bin/false git
+  sudo useradd -r -s /bin/bash git
+fi
+
+if ! sudo cat /etc/sudoers | grep "^git ALL=(phabricator-daemon)" >> /dev/null; then
+  echo "git ALL=(phabricator-daemon) SETENV: NOPASSWD: $(whereis -b git-upload-pack | cut -d' ' -f2-), $(whereis -b git-receive-pack | cut -d' ' -f2-)" | (sudo su -c 'EDITOR="tee -a" visudo')
 fi
