3269610a72dd5b545084e2a2cec21cd2aa66d68a
Jeff Verkoeyen <featherless@google.com>
2015-11-27 11:45:21 -0500

    Hosted repos must be owned by the phabricator-daemon.

--- vm/configure_hosted_repos.sh
+++ vm/configure_hosted_repos.sh
@@ -2,5 +2,5 @@
 
 if [ ! -d /var/repo/ ]; then
   echo "Creating hosted repo folder..."
-  mkdir -p /var/repo && chown www-data:www-data /var/repo
+  sudo mkdir -p /var/repo && sudo chown phabricator-daemon:phabricator-daemon /var/repo
 fi
