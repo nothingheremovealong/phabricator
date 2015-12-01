5bcdef380df284fa3a196076021f68b9a1133e72
Jeff Verkoeyen <featherless@google.com>
2015-11-30 23:01:16 -0500

    Require sudo to modify config.

--- vm/configure_mailgun.sh
+++ vm/configure_mailgun.sh
@@ -12,11 +12,11 @@ pushd phabricator >> /dev/null
 
 echo "Configuring Phabricator for Mailgun..."
 
-./bin/config set mailgun.api-key $MAILGUN_APIKEY
-./bin/config set mailgun.domain $PHABRICATOR_BASE_DOMAIN
+sudo ./bin/config set mailgun.api-key $MAILGUN_APIKEY
+sudo ./bin/config set mailgun.domain $PHABRICATOR_BASE_DOMAIN
 
-./bin/config set --database metamta.mail-adapter PhabricatorMailImplementationMailgunAdapter
-./bin/config set --database metamta.domain $PHABRICATOR_BASE_DOMAIN
-./bin/config set --database metamta.default-address noreply@$PHABRICATOR_BASE_DOMAIN
+sudo ./bin/config set --database metamta.mail-adapter PhabricatorMailImplementationMailgunAdapter
+sudo ./bin/config set --database metamta.domain $PHABRICATOR_BASE_DOMAIN
+sudo ./bin/config set --database metamta.default-address noreply@$PHABRICATOR_BASE_DOMAIN
 
 popd >> /dev/null
