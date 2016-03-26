ebd1b9f73816b27b158223c184e3d55410093982
Jeff Verkoeyen <jverkoey@gmail.com>
2016-03-26 15:01:35 -0400

    More sudo.

--- vm/configure_apache.sh
+++ vm/configure_apache.sh
@@ -7,13 +7,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 function safe_copy {
   if [ ! -f $2 ]; then
     echo "Copying $1..."
-    cp $1 $2
+    sudo cp $1 $2
   else
     if ! cmp --silent $1 $2; then
       echo "Overwrite existing $1 at $2?"
       select yn in "Yes" "No"; do
         case $yn in
-          Yes ) echo "Updating $2 site...";cp $1 $2; break;;
+          Yes ) echo "Updating $2 site...";sudo cp $1 $2; break;;
           No ) exit;;
         esac
       done
