a42bf496dc5bdba86ef1c1b75673853ead124246
Jeff Verkoeyen <jverkoey@gmail.com>
2016-03-26 14:59:30 -0400

    More sudo.

--- vm/configure_apache.sh
+++ vm/configure_apache.sh
@@ -21,28 +21,28 @@ function safe_copy {
 
 if [ $(grep -c "^Listen 80$" /etc/apache2/ports.conf) -ne 0  ]; then
   echo "Listening port set to 8080.";
-  sed -i -e 's/^Listen 80$/Listen 8080/' /etc/apache2/ports.conf
+  sudo sed -i -e 's/^Listen 80$/Listen 8080/' /etc/apache2/ports.conf
 fi
 
 if [ -f /etc/apache2/sites-enabled/000-default.conf ]; then
   echo "Removing default site..."
-  rm -f /etc/apache2/sites-enabled/000-default.conf
+  sudo rm -f /etc/apache2/sites-enabled/000-default.conf
 fi
 
 safe_copy $DIR/sites/phabricator.conf /etc/apache2/sites-available/phabricator.conf
 
 if [ ! -h /etc/apache2/sites-enabled/phabricator.conf ]; then
   echo "Activating phabricator site..."
-  ln -s /etc/apache2/sites-available/phabricator.conf /etc/apache2/sites-enabled/phabricator.conf
+  sudo ln -s /etc/apache2/sites-available/phabricator.conf /etc/apache2/sites-enabled/phabricator.conf
 fi
 
 if [ ! -d /usr/local/apache/logs ]; then
   echo "Configuring apache logs..."
-  mkdir -p /usr/local/apache/logs && chown www-data:www-data /usr/local/apache/logs
+  sudo mkdir -p /usr/local/apache/logs && chown www-data:www-data /usr/local/apache/logs
 fi
 
 if [ ! -d /var/log/phabricator ]; then
   echo "Configuring phabricator logs..."
-  mkdir -p /var/log/phabricator && chown www-data:www-data /var/log/phabricator
+  sudo mkdir -p /var/log/phabricator && chown www-data:www-data /var/log/phabricator
 fi
 
