477c9c892a74993c3948d646b6b6043a92e0a959
Jeff Verkoeyen <jverkoey@gmail.com>
2016-03-26 15:02:10 -0400

    More sudo.

--- vm/configure_php.sh
+++ vm/configure_php.sh
@@ -5,7 +5,7 @@ set -e
 function disable_php {
   if [ $(grep -c "^$1" /etc/php5/apache2/php.ini) = 0 ]; then
     echo "Disabling $1..."
-    echo "$1 = 0" >> /etc/php5/apache2/php.ini
+    sudo echo "$1 = 0" >> /etc/php5/apache2/php.ini
   fi
 }
 
@@ -15,6 +15,6 @@ disable_php opcache.validate_timestamps
 
 if [ $(grep -c "^post_max_size = 8M$" /etc/php5/apache2/php.ini) -ne 0  ]; then
   echo "Increasing post max size to 32M.";
-  sed -i -e "s/post_max_size = 8M/post_max_size = 32M/" /etc/php5/apache2/php.ini
+  sudo sed -i -e "s/post_max_size = 8M/post_max_size = 32M/" /etc/php5/apache2/php.ini
 fi
 
